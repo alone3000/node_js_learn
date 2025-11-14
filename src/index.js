f4c008a677d9c36da312024f34485a78f22f5f3b
MrXAlone1 <mr.xpro8315@gmail.com>
2025-11-14 13:18:48 +0530

    node static files setup

--- src/index.js
+++ src/index.js
@@ -1,44 +1,54 @@
 // // commonjs statement
-let http = require('http') 
+// let http = require('http') 
 
 // modulejs statement import is used in production
 // import http from 'http'
 
-let server = http.createServer((req,res)=>{
- res.end("<h1 style='text-align:center;color:red;'>HI everyone,I am back</h1>")
-})
+// let server = http.createServer((req,res)=>{
+//  res.end("<h1 style='text-align:center;color:red;'>HI everyone,I am back</h1>")
+// })
 
-server.listen(3000)
+// server.listen(3000)
 
 
 
 
 // Expressjs
 
-// import express from 'express'
-// import path from 'path'
+import express from 'express'
+import path from 'path'
 // import { fileURLToPath } from 'url'
 
-// const app = express()
+const app = express()
 
 // // const __filename = fileURLToPath(import.meta.url)
 // // const __dirname = path.dirname(__filename)
 
 // //or
 
-// const __dirname = import.meta.dirname
+const __dirname = import.meta.dirname
+
+//always focus on package.json file , for relative path not server file
+app.use('/static',express.static('public'))
+app.use('/assets',express.static('./src/assets'))
 
 // console.log([__dirname,import.meta])
 
-// app.get('/',(req,res)=>{
-//  res.send("<h1 align='center'>welcome to express js</h1>")
-// })
+app.get('/',(req,res)=>{
+ res.send("<h1 align='center'>welcome to express js</h1>")
+})
 
-// app.get('/home',(req,res)=>{
-//     // console.log(path)
-//     res.sendFile(path.join(__dirname,'./views/index.html'))
-// })
+app.get('/home',(req,res)=>{
+    // console.log(path)
+    res.sendFile(path.join(__dirname,'./views/index.html'))
+})
 
-// app.listen(5000,(req,res)=>{
-// console.log("server listen on port 5000")
-// })
+
+app.post('/formdata',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/form.html'))
+})
+
+
+app.listen(5000,(req,res)=>{
+console.log("server listen on port 5000")
+})
